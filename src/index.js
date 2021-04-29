78d2423a5ab61e136296167560eedf4731321533
Ayushi Verma <50084909+Ayushi20-19@users.noreply.github.com>
2021-04-29 14:36:03 +0530

    commit

--- src/index.js
+++ src/index.js
@@ -1,9 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import Card from './Card';
 
 ReactDOM.render(
- <><h1>hello</h1></>,
+ <>
+   <Card imgsrc=""
+     title="Netflix"
+     sname="Vampire Diaries"
+     link=""
+   />
+    <Card imgsrc=""
+     title="Netflix"
+     sname="Dark"
+     link=""
+   />
+    <Card imgsrc=""
+     title="Netflix"
+     sname="F.R.I.E.N.D.S"
+     link=""
+   />
+    <Card imgsrc=""
+     title="Netflix"
+     sname="Nauruto"
+     link=""
+   />
+    <Card imgsrc=""
+     title=""
+     sname=""
+     link=""
+   />
+ </>,
   document.getElementById('root')
 );
 
