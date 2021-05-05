145ddc002b9a403a20e8ac00856efe5c427e70f6
Ayushi Verma <50084909+Ayushi20-19@users.noreply.github.com>
2021-05-05 20:24:13 +0530

    main

---
+++ src/Sdata.jsx
@@ -0,0 +1,55 @@
+const Sdata = [
+    {
+        sname: "F.R.I.E.N.D.S",
+        imgsrc: " ",
+        title:"",
+        links:"",     
+
+},
+{
+    sname: "F.R.I.E.N.D.S",
+    imgsrc: " ",
+    title:"",
+    links:"",     
+
+},
+{
+    sname: "F.R.I.E.N.D.S",
+    imgsrc: " ",
+    title:"",
+    links:"",     
+
+},
+{
+    sname: "F.R.I.E.N.D.S",
+    imgsrc: " ",
+    title:"",
+    links:"",     
+
+},{
+    sname: "F.R.I.E.N.D.S",
+    imgsrc: " ",
+    title:"",
+    links:"",     
+
+},{
+    sname: "F.R.I.E.N.D.S",
+    imgsrc: " ",
+    title:"",
+    links:"",     
+
+},{
+    sname: "F.R.I.E.N.D.S",
+    imgsrc: " ",
+    title:"",
+    links:"",     
+
+},{
+    sname: "F.R.I.E.N.D.S",
+    imgsrc: " ",
+    title:"",
+    links:"",     
+
+},
+]  
+export default Sdata;
